9320e8ad3d152350d4e3f859efc87ee6a3da3770
Macradon <36033419+Macradon@users.noreply.github.com>
2019-05-31 16:50:48 +0200

    Added models, and a lot to the UI

---
+++ Eksamensforberedelse1/Eksamensforberedelse1/Eksamensforberedelse1/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eksamensforberedelse1.Models
+{
+    class Item
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+        public int tier { get; set; }
+        public string enhancements { get; set; }
+        public string description { get; set; }
+    }
+}
